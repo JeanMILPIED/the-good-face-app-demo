fab5c4850495b5f012f4b05aa2b4b1fc48afa9ad
JEAN <56645415+JeanMILPIED@users.noreply.github.com>
2023-01-23 00:30:13 +0100

    beta version of the good face app demo (works local)

--- the-good-face-app-demo.py
+++ the-good-face-app-demo.py
@@ -13,39 +13,52 @@ def portraitImage_optimisation(image_name):
     else:
         return 'error'
 
-st.title('The-good-face-app')
-st.header('demo code')
+st.title('The-good-face-app 📷😎 (demo)')
+st.header("Make a Linkedin portrait from your best shots")
+st.subheader('Our AI will rate it and propose a better rated one')
+
 portrait_img=None
 
-col1, col2=st.columns(2)
 source = st.radio(
     "Where is your portrait picture source ?",
     ('File','Webcam'))
 
+
 if source=='File':
     portrait_img=st.file_uploader('Chose a portrait picture')
+    col1, col2 = st.columns(2)
     if portrait_img!=None:
         bytes_data = portrait_img.getvalue()
         # Show the image filename and image.
-        st.write(f'filename: {portrait_img.name}')
-        st.image(bytes_data)
         with open('portrait.jpg', "wb") as f:
             f.write(bytes_data)
+        col1.write('your initial portrait')
+        col1.image(bytes_data)
 
 if source=='Webcam':
     portrait_img = st.camera_input('Take a new webcam picture')
+    col1, col2 = st.columns(2)
     if portrait_img!=None:
         bytes_data = portrait_img.getvalue()
         # Show the image filename and image.
-        st.write(f'filename: {portrait_img.name}')
-        st.image(bytes_data)
         with open('portrait.jpg', "wb") as f:
             f.write(bytes_data)
+        col1.write('Your initial portrait')
+        col1.image(bytes_data)
+
+
 
 if portrait_img !=None:
-    total_proba_initial, total_proba_final = auto_portraitImage_optimisation('portrait')
-    st.header('YOUR best portrait with quality probability')
-    st.image('portrait_ALLcorrected.jpg')
+    try:
+        total_proba_initial, total_proba_final = auto_portraitImage_optimisation('portrait')
+        col2.write('Your best portrait with quality probability')
+        col2.image('portrait_ALLcorrected.jpg')
+
+        #write probas
+        col1.write('We rate it at {}'.format(total_proba_initial))
+        col2.write('Our best work is now rated at {}'.format(total_proba_final))
+    except:
+        st.write("Oups! Something went wrong")
 
 
 
